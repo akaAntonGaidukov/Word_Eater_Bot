7d5419f0ef3b25de1c2e28b4032242222cf2b788
akaAntonGaidukov <gaidukov.nvt@gmail.com>
2023-03-17 20:16:26 +0300

    x

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 
 
-TG_TOKEN = '5652546910:AAHx3kZ_5TmqT6N0yp3-nwSTZvC1RVl5LuY'
-BETA_TG_TOKEN = '6208023925:AAFocsyzWMn65I191xFeR5BtRvtU1s0zidE'
-mongo_user = 'bluebear'
-mongo_pass = 'gOk9wOhwiuGpxqeP'
+TG_TOKEN = ""
+BETA_TG_TOKEN = ""
+mongo_user = ""
+mongo_pass = ""
 
-mongo_api = 'hRmvapcRL9IHkVIMWAIw98Ez6DcANZhh6J9VFlmYOJKzclV8wwkKZWVLOKGRWq9O'
+mongo_api = ""
